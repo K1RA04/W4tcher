b05cb593c0116683b9718a91db6adcc4ecfc8689
K1RA04 <raphael.kindlinger04@gmail.com>
2024-04-14 13:54:07 +0200

    Added a Bug-Issue creator. renamed the bugs file.
    To keep track of ongoing bugs and their errors

---
+++ BugIssuecreator.py
@@ -0,0 +1,22 @@
+import datetime
+
+def count_bugs(filename):
+    with open(filename, 'r') as file:
+        return sum(1 for line in file if line.startswith('Bug'))
+
+def add_bug_report(filename, bug_desc, resolved):
+    bug_number = count_bugs(filename) + 1
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(filename, 'a') as file:
+        file.write('--------------------------------\n')
+        file.write(f'Bug{bug_number}\n')
+        file.write(f'Description: {bug_desc}\n')
+        file.write(f'Date of Notice: {current_time}\n')
+        file.write(f'Resolved: {resolved}\n')
+        file.write('--------------------------------\n\n')
+
+# Example usage:
+bug_desc = input("Description: ")
+resolved = input("Resolved? (YES/NO): ")
+
+add_bug_report('bugs.txt', bug_desc, resolved.upper())
